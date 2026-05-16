ff8c07769dcd0db939868745ad390e3a4b40074b
smartinvestsi254-prog <smartinvestsi254@gmail.com>
2026-05-16 15:04:27 +0300

    Update env.server.ts

--- src/config/env.server.ts
+++ src/config/env.server.ts
@@ -3,7 +3,8 @@ import { env } from "./env";
 /**
  * Ensure this file is never imported in frontend bundles
  */
-if (typeof window !== "undefined") {
+
+if (typeof process !== 'undefined' && process.versions && process.versions.node) {
   throw new Error("❌ env.server.ts should not be used in the browser");
 }
 
